17cf139fdde7f2aac6eb8abcbe29c9a8d6ed8615
EricXiaoYang <51061647+EricXiaoYang@users.noreply.github.com>
2021-10-23 00:34:20 -0400

    basic table components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import Home from './Home';
 import Header from './Header';
 import UserProfile from './UserProfile'
+import Tablelist from './TableList/Tablelist';
+import Tablecreate from './TableList/Tablecreate';
 
 //boolean that determines whether or not User Profile button appear in header.
 //will depend on to-be-implemented logic (e.g. disappears on User Profile page)
@@ -24,6 +26,16 @@ const App = (props) => {
           <Route path="/user">
             <UserProfile title="User Profile | All In Poker"/>
           </Route>
+
+          <Route path="/tablelist">
+            <Tablelist title = "Table List"/>
+          </Route>
+
+          <Route path="/tablecreate">
+            <Tablecreate title = "Table Create"/>
+          </Route>
+
+
         </Switch>
       </Router>
     </div>
---
+++ front-end/src/TableList/TableBlock.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Row } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+
+// import Stack from 'react-bootstrap/Stack'
+
+const TableBlock = ( {table} ) => {
+    return (
+        // <Stack direction = "horizontal" gap={3}  style={{"border": "1px solid"}}>
+            
+        //     <div className="bg-light border">props.name</div>
+        //     <div className="bg-light border">props.curPlayers/props.numPlayers</div>
+        //     <div className="bg-light border">props.gameType</div>
+        //     <div className="bg-light border">props.status</div>
+
+        // </Stack>
+
+        <Container className = "bg-gray border">
+            <Row>
+                <Col className="bg-gray border"> table.name </Col>
+                <Col className="bg-gray border"> table.curPlayers/table.totalPlayers </Col>
+                <Col className="bg-gray border"> table.gameType </Col>
+                <Col className="bg-gray border"> table.status </Col>
+                
+            </Row>
+        </Container>
+
+
+    )
+}
+
+export default TableBlock
