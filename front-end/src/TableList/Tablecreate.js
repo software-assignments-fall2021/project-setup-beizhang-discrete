49787d371fb18b76f9ef4a0f1260637127734faa
oooscarrr <rx513@nyu.edu>
2021-10-25 18:05:00 -0400

    Invite Friend Overlay

--- front-end/src/TableList/Tablecreate.js
+++ front-end/src/TableList/Tablecreate.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, ReactDOM  } from 'react'
 import './Tablelist.css'
 import Container from 'react-bootstrap/Container'
 import { Col, Form, Row } from 'react-bootstrap'
@@ -6,13 +6,49 @@ import Button from '@restart/ui/esm/Button'
 
 // import TableBlock from './TableBlock'
 
-
+const FriendListItem= (props) => {
+    return(
+        <div className='FriendItem'>
+            <img className='FriendPhoto' src={'./profileicon.png'} alt={'Friend Photo'} />
+            <p className='FriendName'>{props.name}</p>
+            <p className='FriendStatus'>{props.status}</p>
+            <Button>Invite</Button>
+        </div>
+    )
+}
 
 const Tablecreate = (props) => {
+
     useEffect(() => {
         props.updateUserProfileButton(true);
     }, [props]);
-    return (
+
+    function openOverlay() {
+        ReactDOM.render(<InviteFriendOverlay/>, document.getElementById('root'))
+    }
+      
+    function closeOverlay() {
+        
+    }
+
+    const InviteFriendOverlay = (props) =>{
+        return(
+            <div className="InviteOverlay">
+                <a href="javascript:void(0)" className="closebtn" onClick={closeOverlay}>&times;</a>
+                <div className="FriendsToInvite">
+                    <FriendListItem name={<Button>Owen</Button>} status='Playing'/>
+                    <FriendListItem name={<Button>Thomas</Button>} status='Online'/>
+                    <FriendListItem name={<Button>Eric</Button>} status='Away'/>
+                    <FriendListItem name={<Button>Ben</Button>} status='Offline'/>
+                    <FriendListItem name={<Button>Oscar</Button>} status='Offline'/>
+                </div>
+            </div>
+        )
+    }
+
+    return (       
+        <div id='root'>
+
         <Container className="bg-white border" fluid="md">
             <h1> Create Table</h1>
             <h4> # of Players</h4>
@@ -69,7 +105,14 @@ const Tablecreate = (props) => {
                 </Button>
             </a>
 
+            <button onClick={openOverlay}>
+                Invite Friend
+            </button>
+
         </Container>
+
+
+        </div>
     )
 }
 
