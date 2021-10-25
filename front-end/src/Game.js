cf7b7d04086208af0a608e8531ccead09255f02c
Ben <BPasteelnick@gmail.com>
2021-10-25 19:03:53 -0400

    added center cards and deck management

--- front-end/src/Game.js
+++ front-end/src/Game.js
@@ -12,17 +12,6 @@ for (const val of cardValues){
     }
 }
 let gameDeck = [...deck]
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-
-
-
-
->>>>>>> b09a695 (center cards and proper deck management)
-=======
->>>>>>> 455fdaa (added center cards and deck management)
 function Game(props) {
     const [action, setAction] = useState("")
     const [handFirst, setHandFirst] = useState("")
@@ -33,13 +22,6 @@ function Game(props) {
     const [turn, setTurn] = useState("")
     const [river, setRiver] = useState("")
     const [rulesText, setRulesText] = useState("Preflop Betting.")
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    console.log("setting gamephase from start")
->>>>>>> b09a695 (center cards and proper deck management)
-=======
->>>>>>> 455fdaa (added center cards and deck management)
     const [gamePhase, setGamePhase] = useState(0)
     //Game Phases:
     // 0: Preflop betting (2 cards in hand)
@@ -57,10 +39,6 @@ function Game(props) {
     }, []);
 
     useEffect(() => {
-<<<<<<< HEAD
->>>>>>> b09a695 (center cards and proper deck management)
-=======
->>>>>>> 455fdaa (added center cards and deck management)
         switch(gamePhase){
             case 1:
                 setFlopFirst(getCard())
@@ -76,14 +54,6 @@ function Game(props) {
                 setRiver(getCard())
                 setRulesText("The river! Last round of bets!")
                 break;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-            default:
-                console.log("error! gamephase: " + gamePhase)
->>>>>>> b09a695 (center cards and proper deck management)
-=======
->>>>>>> 455fdaa (added center cards and deck management)
         }
     }, [gamePhase])
 
@@ -96,14 +66,6 @@ function Game(props) {
             [gameDeck[currentIndex], gameDeck[randomIndex]] = [
             gameDeck[randomIndex], gameDeck[currentIndex]];
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        
-        console.log(gameDeck)
->>>>>>> b09a695 (center cards and proper deck management)
-=======
->>>>>>> 455fdaa (added center cards and deck management)
     }
 
     const getCard = () => {
@@ -120,13 +82,6 @@ function Game(props) {
         setAction("Called")
         if (gamePhase < 3){
             setGamePhase(gamePhase+1)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-            console.log("game phase: " + gamePhase)
->>>>>>> b09a695 (center cards and proper deck management)
-=======
->>>>>>> 455fdaa (added center cards and deck management)
         }
     }
     const raise = () => {
@@ -142,17 +97,6 @@ function Game(props) {
         setGamePhase(0)
         shuffleDeck()
         drawHand()
-<<<<<<< HEAD
-=======
-        console.log("setting gamephase from fold")
-        setGamePhase(0)
-        shuffleDeck()
-        drawHand()
-        console.log(handFirst + handSecond)
-
->>>>>>> b09a695 (center cards and proper deck management)
-=======
->>>>>>> 455fdaa (added center cards and deck management)
     }
 
     return (
