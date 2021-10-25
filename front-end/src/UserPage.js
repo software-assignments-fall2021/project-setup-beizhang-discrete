8a77e94c7fe962fff5bd7a28ad9a374d17e07e1b
oooscarrr <rx513@nyu.edu>
2021-10-25 02:28:37 -0400

    User Page

--- front-end/src/UserPage.js
+++ front-end/src/UserPage.js
@@ -1,6 +1,17 @@
 import React, {useEffect} from 'react';
 import './UserPage.css';
-/* import profileicon from './profileicon.png'; */
+import Button from 'react-bootstrap/Button';
+
+const FriendListItem= (props) => {
+    return(
+        <div className='FriendItem'>
+            <img className='FriendPhoto' src={'./profileicon.png'} alt={'Friend Photo'} />
+            <p className='FriendName'>{props.name}</p>
+            <p className='FriendStatus'>{props.status}</p>
+            <Button>Invite</Button>
+        </div>
+    )
+}
 
 function UserPage(props) {
     
@@ -9,20 +20,35 @@ function UserPage(props) {
     }, [props.title]);
 
     return (
-        <div className="UserProfile">
+        <div className="UserPage">
 
             <div className='PhotoName'>
-            <img className='ProfileIcon' src={'./profileicon.png'} alt={'Profile Icon'} />
-            <h1 className='Username'>Username</h1>
+                <img className='ProfilePhoto' src={'./profileicon.png'} alt={'Profile Icon'} />
+                <h1 className='Username'>Username</h1>
             </div>
 
-            <div className='UserStats'>
-                <center><h2>User Stats</h2></center>
-                <div className='StatsBox'>
-                    <p>Joined since: {props.datejoined}</p>
-                    <p>Games played: {props.gamesplayed}</p>
-                    <p>Games won: {props.gameswon}</p>
-                </div>
+            <h2 className='UserStats'>User Stats</h2>
+            <div className='StatsBox'>
+                <p>Joined since: {props.datejoined}</p>
+                <p>Games played: {props.gamesplayed}</p>
+                <p>Games won: {props.gameswon}</p>
+            </div>
+
+            <h3 className='FriendList'>Friend List</h3>
+            <div className='FriendListBox'>
+                <FriendListItem name={<Button>Owen</Button>} status='Playing'/>
+                <FriendListItem name={<Button>Thomas</Button>} status='Online'/>
+                <FriendListItem name={<Button>Eric</Button>} status='Away'/>
+                <FriendListItem name={<Button>Ben</Button>} status='Offline'/>
+                <FriendListItem name={<Button>Oscar</Button>} status='Offline'/>
+            </div>
+
+            <div className='AddFriend'>
+                <h4>Add Friend</h4>
+                <form>
+                    <input type="SearchUsername" placeholder={'Username'}/>
+                    <input type='submit' value='Search'/>
+                </form>
             </div>
         </div>
     )
--- front-end/src/UserProfile.js
+++ front-end/src/UserProfile.js
@@ -1,6 +1,8 @@
 import Button from '@restart/ui/esm/Button';
 import React, {useEffect, useState} from 'react'
+import { NavLink } from 'react-router-dom';
 import './UserProfile.css';
+import UserPage from './UserPage';
 
 const UserProfile = (props) => {
 
@@ -18,40 +20,48 @@ const UserProfile = (props) => {
     const returnProfile = () => {
         //User profile page
         return (
-            <h4>User Page</h4>
+            <UserPage title="User Page | All In Poker"/>
         )
     }
 
     const returnLoginOrCreate = () => {
         //Login or Create Account
         return (
-            <div className="UserProfile" style={{alignItems: 'center', textAlign: 'center'}}>
-                <h4 style={{color: 'white', margin: '6vh'}}>Login to Existing Account</h4>
-    
-                <form style={{display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: '6vh'}}>
-                    <label style={{padding: '10px'}}>    
-                        <input type="text" placeholder={'Email'} onChange = {change => setEmail(change.target.value)}/>
-                    </label>
-                    <label style={{padding: '10px'}}>    
-                        <input type="password" placeholder={'Password'} onChange = {change => setPassword(change.target.value)}/>
-                    </label>
-                </form>
-                <Button onClick={() => {handleLogin(email, password)}}>Login</Button>
-                <h4 style={{color: 'white', margin: '6vh'}}>Create Account</h4>
-    
-                <form style={{display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: '3vh'}}>
-                    <label style={{padding: '10px'}}>    
-                        <input type="text" placeholder={'Email'} onChange = {change => setEmail(change.target.value)}/>
-                    </label>
-                    <label style={{padding: '10px'}}>    
-                        <input type="password" placeholder={'Password'} onChange = {change => setPassword(change.target.value)}/>
-                    </label>
-                    <label style={{padding: '10px'}}>    
-                        <input type="password" placeholder={'Confirm Password'} onChange = {change => setConfirmPassword(change.target.value)}/>
-                    </label>
-                </form>
-                <Button onClick={() => {handleCreate(email, password, confirmPassword)}}>Create Account</Button>
+            <div>
+                <div className="UserProfile" style={{alignItems: 'center', textAlign: 'center'}}>
+                    <h4 style={{color: 'white', margin: '6vh'}}>Login to Existing Account</h4>
+        
+                    <form style={{display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: '6vh'}}>
+                        <label style={{padding: '10px'}}>    
+                            <input type="text" placeholder={'Email'} onChange = {change => setEmail(change.target.value)}/>
+                        </label>
+                        <label style={{padding: '10px'}}>    
+                            <input type="password" placeholder={'Password'} onChange = {change => setPassword(change.target.value)}/>
+                        </label>
+                    </form>
+                    <Button onClick={() => {handleLogin(email, password)}}>Login</Button>
+                    <h4 style={{color: 'white', margin: '6vh'}}>Create Account</h4>
+        
+                    <form style={{display: 'flex', alignItems: 'center', flexDirection: 'column', marginTop: '3vh'}}>
+                        <label style={{padding: '10px'}}>    
+                            <input type="text" placeholder={'Email'} onChange = {change => setEmail(change.target.value)}/>
+                        </label>
+                        <label style={{padding: '10px'}}>    
+                            <input type="password" placeholder={'Password'} onChange = {change => setPassword(change.target.value)}/>
+                        </label>
+                        <label style={{padding: '10px'}}>    
+                            <input type="password" placeholder={'Confirm Password'} onChange = {change => setConfirmPassword(change.target.value)}/>
+                        </label>
+                    </form>
+                    <Button onClick={() => {handleCreate(email, password, confirmPassword)}}>Create Account</Button>
+                </div>
+
+                <a href='/userpage'>
+                    <Button>See User Page</Button>
+                </a>
+
             </div>
+
         )
     }
     
