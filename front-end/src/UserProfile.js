b7a2ca8206fe216b91933194d175ecc8c78fb376
oooscarrr <rx513@nyu.edu>
2021-10-24 20:33:04 -0400

    User Profile

--- front-end/src/UserProfile.js
+++ front-end/src/UserProfile.js
@@ -1,14 +1,28 @@
-import React, {useEffect} from 'react'
+import React, {useEffect} from 'react';
 import './UserProfile.css';
+/* import profileicon from './profileicon.png'; */
 
 function UserProfile(props) {
     useEffect(() => {
         document.title = props.title || "";
     }, [props.title]);
+
     return (
         <div className="UserProfile">
-            {/* user profile page */}
-            <center><p>user profile page goes here</p></center>
+
+            <div className='PhotoName'>
+            <img className='ProfileIcon' src={'./profileicon.png'} alt={'Profile Icon'} />
+            <h1 className='Username'>Username</h1>
+            </div>
+
+            <div className='UserStats'>
+                <center><h2>User Stats</h2></center>
+                <div className='StatsBox'>
+                    <p>Joined since: {props.datejoined}</p>
+                    <p>Games played: {props.gamesplayed}</p>
+                    <p>Games won: {props.gameswon}</p>
+                </div>
+            </div>
         </div>
     )
 }
