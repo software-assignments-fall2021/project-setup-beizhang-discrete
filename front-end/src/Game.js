b09a695c71853378419216476362fac65d5db3d7
Ben <BPasteelnick@gmail.com>
2021-10-25 18:33:51 -0400

    center cards and proper deck management

--- front-end/src/Game.js
+++ front-end/src/Game.js
@@ -2,11 +2,21 @@ import React, {useState, useEffect} from 'react';
 import './Game.css';
 import Chat from './chat/Chat'
 
-function Game(props) {
-    useEffect(() => {
-        document.title = props.title || "";
-    }, [props.title]);
+const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+const cardSuits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
 
+const deck = []
+for (const val of cardValues){
+    for (const suit of cardSuits){
+        deck.push(val + " of " + suit)
+    }
+}
+let gameDeck = [...deck]
+
+
+
+
+function Game(props) {
     const [action, setAction] = useState("")
     const [handFirst, setHandFirst] = useState("")
     const [handSecond, setHandSecond] = useState("")
@@ -15,21 +25,60 @@ function Game(props) {
     const [flopThird, setFlopThird] = useState("")
     const [turn, setTurn] = useState("")
     const [river, setRiver] = useState("")
+    const [rulesText, setRulesText] = useState("Preflop Betting.")
+    console.log("setting gamephase from start")
+    const [gamePhase, setGamePhase] = useState(0)
+    //Game Phases:
+    // 0: Preflop betting (2 cards in hand)
+    // 1: Flop (3 cards in center)
+    // 2: Turn (4 cards in center)
+    // 3: River (5 cards in center. Last round of betting.)
 
-    const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    const cardSuits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+    useEffect(() => {
+        document.title = props.title || "";
+    }, [props.title]);
 
-    const deck = []
-    for (const val of cardValues){
-        for (const suit of cardSuits){
-            deck.push(val + " of " + suit)
+    useEffect(() => {
+        shuffleDeck()
+        drawHand()
+    }, []);
+
+    useEffect(() => {
+        switch(gamePhase){
+            case 1:
+                setFlopFirst(getCard())
+                setFlopSecond(getCard())
+                setFlopThird(getCard())
+                setRulesText("The flop comes down!")
+                break;
+            case 2:
+                setTurn(getCard())
+                setRulesText("Here comes the turn!")
+                break;
+            case 3:
+                setRiver(getCard())
+                setRulesText("The river! Last round of bets!")
+                break;
+            default:
+                console.log("error! gamephase: " + gamePhase)
         }
+    }, [gamePhase])
 
+    const shuffleDeck = () => {
+        gameDeck = [...deck]
+        let currentIndex = gameDeck.length,  randomIndex;
+        while (currentIndex != 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [gameDeck[currentIndex], gameDeck[randomIndex]] = [
+            gameDeck[randomIndex], gameDeck[currentIndex]];
+        }
+        
+        console.log(gameDeck)
     }
 
     const getCard = () => {
-        let r = deck.shift()
-        console.log("r = " + deck[0])
+        let r = gameDeck.shift()
         return r
     }
 
@@ -40,19 +89,35 @@ function Game(props) {
 
     const call = () => {
         setAction("Called")
+        if (gamePhase < 3){
+            setGamePhase(gamePhase+1)
+            console.log("game phase: " + gamePhase)
+        }
     }
     const raise = () => {
         setAction("Raised")
     }
     const fold = () => {
         setAction("Folded")
+        setFlopFirst("")
+        setFlopSecond("")
+        setFlopThird("")
+        setTurn("")
+        setRiver("")
+        console.log("setting gamephase from fold")
+        setGamePhase(0)
+        shuffleDeck()
         drawHand()
         console.log(handFirst + handSecond)
+
     }
 
     return (
         <div>
             <center>
+                <p>
+                    Center Cards: {flopFirst}, {flopSecond}, {flopThird}, {turn}, {river}
+                </p>
                 <p>
                     Your Hand has: {handFirst} {handSecond}
                 </p>
