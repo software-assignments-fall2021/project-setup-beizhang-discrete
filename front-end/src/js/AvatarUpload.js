b8351550b983efb89191f9470904a77d64f102bd
Thomas Huang <thomashuang02@gmail.com>
2021-11-02 16:05:19 -0400

    avatar upload without db integration

--- front-end/src/js/AvatarUpload.js
+++ front-end/src/js/AvatarUpload.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import '../css/AvatarUpload.css';
 
 const AvatarUpload = (props) => {
+    const api = "http://localhost:4000"
     return (
         <div className='container'>
-            <form className='upload-form' action="/upload-avatar" method="POST" enctype="multipart/form-data">
+            <form className='upload-form' action={api + "/upload-avatar"} method="POST" enctype="multipart/form-data">
                 <input name="avatar" type="file" />
                 <input type="submit" value="Upload" />
             </form>
