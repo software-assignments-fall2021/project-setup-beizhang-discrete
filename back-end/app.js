4cc05163b996fb837c0ea0b2e51dea70b84c01c5
Thomas Huang <thomashuang02@gmail.com>
2021-11-06 16:50:26 -0400

    setup authentication packages, routes, config for next sprint

--- back-end/app.js
+++ back-end/app.js
@@ -8,12 +8,30 @@ const multer = require("multer");
 require("dotenv").config({ silent: true });
 const morgan = require("morgan");
 const axios = require('axios');
+const cors = require('cors');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
 
 /* ------------------------------- middleware ------------------------------- */
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(cors({
+    origin: "http://localhost:3000", //location of react app we're connecting to
+    credentials: true,
+}));
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+}));
+app.use(cookieParser("secret"));
+app.use(passport.initialize());
+app.use(passport.session());
+require('./passportConfig')(passport);
 
 /* ------------------------------ avatar upload ----------------------------- */
 // save file uploads to disk at 'public/uploads'
@@ -39,32 +57,11 @@ const upload = multer({
     }
 });
 
-/* ----------------------------------- api ---------------------------------- */
+/* ------------------------------- api routes ------------------------------ */
 
 const dbURL = "https://my.api.mockaroo.com";
 //mockaroo keys: 428573d0, 1e756d10
 const mockarooAPIKey = '1e756d10';
-
-const mockFriendListAPI = "/friendList.json";
-app.get("/friendList", (req, res) => {
-    axios.get(`${dbURL}${mockFriendListAPI}?key=${mockarooAPIKey}`)
-    .then(axiosResponse => {
-        res.send(axiosResponse.data);
-    }).catch(err => {
-        console.log(err);
-    });
-});
-
-const mockAllUsersListAPI = "/allUsersList.json";
-app.get("/allUsersList", (req, res) => {
-    axios.get(`${dbURL}${mockAllUsersListAPI}?key=${mockarooAPIKey}`)
-    .then(axiosResponse => {
-        res.send(axiosResponse.data);
-    }).catch(err => {
-        console.log(err);
-    });
-});
-
 
 //importing User schema
 const User = require('./schemae/User').User;
@@ -76,6 +73,35 @@ const User = require('./schemae/User').User;
 //     console.log("Mongoose is connected.");
 // });
 
+/* -------------------------- TODO: get/join table -------------------------- */
+
+/* --------------------------- TODO: create table --------------------------- */
+
+/* ---------------------------- TODO: add friend ---------------------------- */
+
+/* ----------------------------- get friend list ---------------------------- */
+const mockFriendListAPI = "/friendList.json";
+app.get("/friendList", (req, res) => {
+    axios.get(`${dbURL}${mockFriendListAPI}?key=${mockarooAPIKey}`)
+    .then(axiosResponse => {
+        res.send(axiosResponse.data);
+    }).catch(err => {
+        console.log(err);
+    });
+});
+
+/* --------------------------- get all users list --------------------------- */
+const mockAllUsersListAPI = "/allUsersList.json";
+app.get("/allUsersList", (req, res) => {
+    axios.get(`${dbURL}${mockAllUsersListAPI}?key=${mockarooAPIKey}`)
+    .then(axiosResponse => {
+        res.send(axiosResponse.data);
+    }).catch(err => {
+        console.log(err);
+    });
+});
+
+/* ------------------------------ upload avatar ----------------------------- */
 app.post("/uploadAvatar", upload.single("avatar"), (req, res) => {
     // const query = { "_id" : req.body._id };
     // const setAvatar = { $set: { avatar: '/uploads' + req.file.filename } };
@@ -86,9 +112,31 @@ app.post("/uploadAvatar", upload.single("avatar"), (req, res) => {
     res.send('https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg')
 });
 
+/* ----------------------------- authentication ----------------------------- */
+// const authenticate = (req, res, next) => {
+//     passport.authenticate("local", (err, user, info) => {
+//         if (err) throw err;
+//         if (!user) res.send(false);
+//         else {
+//             req.logIn(user, err => {
+//                 if (err) throw err;
+//                 res.send(true);
+//                 console.log(req.user);
+//             });
+//         }
+//     })(req, res, next);
+// }
+
+/* -------------------------------- get user -------------------------------- */
+// app.get("/user", (req, res) => {
+//     res.send(req.user);
+// });
+
+/* ---------------------------------- login --------------------------------- */
 const mockLoginAPI = "/login.json";
-app.post("/login", (req, res) => {
-    const username = req.body.username, password = req.body.password;
+app.post("/login", (req, res, next) => {
+    //authenticate(req, res, next);
+    //const username = req.body.username, password = req.body.password;
     axios.post(`${dbURL}${mockLoginAPI}?key=${mockarooAPIKey}`)
     .then(axiosResponse => {
         res.send(axiosResponse.data);
@@ -97,15 +145,52 @@ app.post("/login", (req, res) => {
     });
 });
 
+/* ----------------------------- create account ----------------------------- */
+const validateRegistration = (username, password, confirmPassword) => {
+    if(username !== username.replace(/<\/?[^>]+(>|$)/g, "")) {
+        return false;
+    }
+    if(password !== confirmPassword) {
+        return false;
+    }
+    return true;
+}
 const mockSignUpAPI = "/createAccount.json";
-app.post("/signUp", (req, res) => {
+app.post("/signUp", (req, res, next) => {
+    // User.findOne({username: req.body.username}, async (err, user) => {
+    //     if (err) throw err;
+    //     if (user) {
+    //         res.send(false);
+    //     }
+    //     else {
+    //         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    //         const newUser = new User({
+    //             username: req.body.username,
+    //             password: hashedPassword,
+    //             list: []
+    //         });
+    //         await newUser.save();
+    //         authenticate(req, res, next);
+    //     }
+    // });
     const username = req.body.username, password = req.body.password, confirmPassword = req.body.confirmPassword;
-    axios.post(`${dbURL}${mockSignUpAPI}?key=${mockarooAPIKey}`)
-    .then(axiosResponse => {
-        res.send(axiosResponse.data);
-    }).catch(err => {
-        console.log(err);
-    });
+    if(validateRegistration(username, password, confirmPassword)) {
+        axios.post(`${dbURL}${mockSignUpAPI}?key=${mockarooAPIKey}`)
+        .then(axiosResponse => {
+            res.send(axiosResponse.data);
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+    else {
+        res.send(false);
+    }
 });
+
+/* --------------------------------- logout --------------------------------- */
+// app.get("/logout", function(req, res){
+//     req.logout();
+//     res.redirect("/");
+// });
 
 module.exports = app;
