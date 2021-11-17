374bd8ba7f63ff46d1b21594dd282d757d1ebbba
Thomas Huang <thomashuang02@gmail.com>
2021-11-17 14:16:40 -0500

    getting rid of some comments no longer needed

--- front-end/src/js/AvatarUpload.js
+++ front-end/src/js/AvatarUpload.js
@@ -5,7 +5,7 @@ const AvatarUpload = (props) => {
     const api = "http://localhost:4000"
     return (
         <div className='container'>
-            <form className='upload-form' action={api + "/upload-avatar"} method="POST" encType="multipart/form-data">
+            <form className='upload-form' action={"/uploadAvatar"} method="POST" encType="multipart/form-data">
                 <input name="avatar" type="file" />
                 <input type="submit" value="Upload" />
             </form>
