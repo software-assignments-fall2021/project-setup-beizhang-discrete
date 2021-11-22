db47ce0f6b42e28878ede4ec8d55f4b50f8a0c36
Thomas Huang <thomashuang02@gmail.com>
2021-11-21 22:17:36 -0500

    fixing some unit tests, client-side requirement of table-create inputs

--- back-end/app.js
+++ back-end/app.js
@@ -3,19 +3,14 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const path = require("path");
-const { v4: uuidv4 } = require('uuid');
 const mongoose = require('mongoose');
 require("dotenv").config({ silent: true });
-const morgan = require("morgan");
-const axios = require('axios');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const Table = require("./schemae/Table");
-const router = express.Router();
 
 /* ------------------------------- middleware ------------------------------- */
-app.use(morgan("dev"));
+//app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/static", express.static(path.join(__dirname, "public")));
@@ -31,10 +26,6 @@ app.use(session({
 app.use(cookieParser("secret"));
 
 /* ------------------------------- api routes ------------------------------ */
-
-const mockarooURL = "https://my.api.mockaroo.com";
-//mockaroo keys: 428573d0, 1e756d10
-const mockarooAPIKey = '428573d0';
 const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@all-in-poker.bsbwv.mongodb.net/allInPoker?retryWrites=true&w=majority`;
 mongoose.connect(dbURI, {
     useNewUrlParser: true,
@@ -53,20 +44,9 @@ app.use(userRoutes);
 const avatarUploadRoutes = require('./routes/avatarUploadRoutes');
 app.use(avatarUploadRoutes);
 
-/* ------------------------------ add friend ----------------------------- */
+/* ------------------------------ friend routes ----------------------------- */
 const addFriendRoutes = require('./routes/addFriendRoutes');
 app.use(addFriendRoutes);
-
-/* ----------------------------- get friend list ---------------------------- */
-const mockFriendListAPI = "/friendList.json";
-app.get("/friendList", (req, res) => {
-    axios.get(`${mockarooURL}${mockFriendListAPI}?key=${mockarooAPIKey}`)
-    .then(axiosResponse => {
-        res.send(axiosResponse.data);
-    }).catch(err => {
-        console.log(err);
-    });
-});
 
 /* --------------------------- table routes --------------------------- */
 const tableRoutes = require('./routes/tableRoutes');
--- front-end/src/js/TableBlock.js
+++ front-end/src/js/TableBlock.js
@@ -53,5 +53,5 @@ const TableBlock = (props) => {
         </Row>
     )
     }
-
+}
 export default TableBlock
