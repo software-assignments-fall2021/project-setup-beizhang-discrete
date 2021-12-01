a39fd08cc597aa0597975fa5670d66c9f37e400e
oooscarrr <rx513@nyu.edu>
2021-12-01 14:14:56 -0500

    Dynamically fetch friendlist and friend requests

--- back-end/routes/addFriendRoutes.js
+++ back-end/routes/addFriendRoutes.js
@@ -3,33 +3,29 @@ const router = express.Router();
 const User = require('../schemae/User').User;
 
 router.post("/sendFriendRequest", async (req, res) => {
-    const [senderName, receiverName] = [req.body.sender, req.body.receiver];
-    if (senderName===receiverName) {
+    const [senderID, receiverID] = [req.body.sender, req.body.receiver];
+    if (senderID===receiverID) {
         res.send('This is yourself!');
         return;
     }
-    const sender = await User.findOne({username: senderName});
-    const receiver = await User.findOne({username: receiverName});
+    const sender = await User.findById(senderID);
+    const receiver = await User.findById(receiverID);
     //If you try to be friend with yourself
 
     //If the other user has already been your friend
-    if (sender.friends.find(friend => friend.username===receiverName)) {
+    if (sender.friends.find(friendID => friendID===receiverID)) {
         res.send("You have already been friends");
     }
     //If you have already sent request to this user waiting for response
-    else if (receiver.friendRequests.find(requester => requester.username===senderName)) {
+    else if (receiver.friendRequests.find(requesterID => requesterID===senderID)) {
         res.send("You have already requested");
     }
-    else if (sender.friendRequests.find(requester => requester.username===receiverName)) {
+    else if (sender.friendRequests.find(requesterID => requesterID===receiverID)) {
         res.send("Look into your friend requests!");
     }
     else {
         //Push request to the receiver's list
-        receiver.friendRequests.push({
-            username: sender.username,
-            avatar: sender.avatar,
-            status: sender.status
-        });
+        receiver.friendRequests.push(senderID);
         receiver.save((err) => {
             if (err) console.log(err);
             else res.send("Friend request successfully sent");
@@ -38,31 +34,18 @@ router.post("/sendFriendRequest", async (req, res) => {
 });
 
 router.post("/acceptFriendRequest", async (req, res) => {
-    const [accepterName, senderName] = [req.body.accepter, req.body.sender];
-    const accepter = await User.findOne({username: accepterName});
-    const sender = await User.findOne({username: senderName});
+    const [accepterID, senderID] = [req.body.accepter, req.body.sender];
+    const accepter = await User.findById(accepterID);
+    const sender = await User.findById(senderID);
     //Add each other into friend lists
-    sender.friends.push({
-        username: accepter.username,
-        avatar: accepter.avatar,
-        status: accepter.status
-    });
+    sender.friends.push(accepterID);
     sender.save((err) => {
         if (err) console.log(err);
     });
 
-    accepter.friends.push({
-        username: sender.username,
-        avatar: sender.avatar,
-        status: sender.status
-    });
+    accepter.friends.push(senderID);
     // accepter.friendRequests.remove
-    for (var i=0; i<accepter.friendRequests.length; i++) {
-        if (accepter.friendRequests[i].username===senderName) {
-            accepter.friendRequests.splice(i, 1);
-            break;
-        }
-    }
+    accepter.friendRequests.splice(accepter.friendRequests.indexOf(senderID), 1);
     accepter.save((err) => {
         if (err) console.log(err);
         else res.send("Friend added");
@@ -70,15 +53,10 @@ router.post("/acceptFriendRequest", async (req, res) => {
 });
 
 router.post("/declineFriendRequest", async (req, res) => {
-    const [declinerName, senderName] = [req.body.decliner, req.body.sender];
-    const decliner = await User.findOne({username: declinerName});
+    const [declinerID, senderID] = [req.body.decliner, req.body.sender];
+    const decliner = await User.findById(declinerID);
     //decliner.friendRequests.remove
-    for (var i=0; i<decliner.friendRequests.length; i++) {
-        if (decliner.friendRequests[i].username===senderName) {
-            decliner.friendRequests.splice(i, 1);
-            break;
-        }
-    }
+    decliner.friendRequests.splice(decliner.friendRequests.indexOf(senderID), 1);
     decliner.save((err) => {
         if (err) console.log(err);
         else res.send("Friend request rejected");
